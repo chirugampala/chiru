454540cd17498493fdbc1f0eba1f58888bd20580
gcr <gcr@gmail.com>
2018-06-21 17:58:40 +0530

    added function

--- chiru.js
+++ chiru.js
@@ -5,6 +5,12 @@
   <script type="text/javascript" src="simple.js"></script>
 </head>
 <body>
+
+  new Promise(function(resolve, reject) {
+    function{
+      document.getElementById('id')
+    }
+  });
   <script type="text/javascript">
     // Exercise the .helloWorld template
     document.write(examples.simple.helloWorld());
